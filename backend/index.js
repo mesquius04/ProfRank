27afcf2d11a05d42aad003375aa5033ee80baaf1
mesquius04 <marti.esquius01@estudiant.upf.edu>
2024-09-01 16:09:36 +0200

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Conectar a MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB', err);
+    });
+
+// Rutas básicas
+app.get('/', (req, res) => {
+    res.send('Hello WWWorld');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
