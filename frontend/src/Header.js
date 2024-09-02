8e798bbed8af8d277daac462675f285662b63db0
mesquius04 <marti.esquius01@estudiant.upf.edu>
2024-09-02 18:06:27 +0200

    Ranking

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import './Header.css';  // Opcional, si quieres agregar estilos
+import './Header.css'; // Si deseas aplicar estilos personalizados
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><a href="#profile">Profile</a></li>
-          <li><a href="#settings">Explore</a></li>
-          <li><a href="#settings">Ranking</a></li>
-          <li><a href="#settings">Settings</a></li>
+    <header className="header">
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <a href="explore" className="navbar-link">Explore</a>
+          </li>
+          <li className="navbar-item">
+            <a href="myspace" className="navbar-link">MySpace</a>
+          </li>
+          <li className="navbar-item">
+            <a href="ranking" className="navbar-link">Ranking</a>
+          </li>
+          <li className="navbar-item">
+            <a href="profile" className="navbar-link">Profile</a>
+          </li>
+          <li className="navbar-item" id='setting'>
+            <a href="settings" className="navbar-link">Settings</a>
+          </li>
         </ul>
       </nav>
     </header>
